fb1fe131ee723870b31dfdc73c3e016bc5ddbca4
antoine <153529541+antoineswg@users.noreply.github.com>
2024-06-09 00:58:40 +0200

    ça avance

--- connexion.php
+++ connexion.php
@@ -1,5 +1,3 @@
 <?php 
-  //pour localhost
-  $db =new PDO('mysql:host=localhost;dbname=resaweb;port8080,charest=utf8', 'root', '');
-
+  $db = new PDO('mysql:host=localhost;dbname=resaweb;port=3306;charset=utf8', 'root', '');
  ?>
---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelector(".invis").addEventListener("click", function () {
+        //Animer le volet pour le dérouler
+        document.querySelector(".invis").animate(
+          [
+            {
+              height: "37em",
+            },
+          ],
+          {
+            duration: 800,
+          }
+        );
+        setTimeout(function f() {
+          //Attribuer la classe vis à la place de la classe invis
+          document.querySelector(".invis").classList.replace("invis", "vis");
+        }, 800);
+      });
+      //Refermer le volet
+      document.querySelector(".basPage").addEventListener("click", function () {
+        document.querySelector(".vis").animate(
+          [
+            {
+              height: "4em",
+            },
+          ],
+          {
+            duration: 800,
+          }
+        );
+        setTimeout(function f() {
+          //Attribuer la classe invis à la place de la classe vis
+          document.querySelector(".vis").classList.replace("vis", "invis");
+        }, 800);
+      });
+
+});
